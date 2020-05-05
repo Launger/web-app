09eebf55476c820f6fe180593ba64b41e8c30108
Richard Guerre <guerre.richard@me.com>
2020-05-05 19:27:05 +0200

    prototype fetch api firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,24 @@
 const functions = require('firebase-functions');
+const fetch = require('node-fetch');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onCall((data, context) => {
+
+  const fakedata = {
+    title: "test",
+    imageURL: "https://i.redd.it/xaj97fukmxw41.jpg"
+  }
+  
+  const fakefetchfromReddit = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(fakedata);
+    }, 1000);
+  })
+
+  return fetch("https://www.reddit.com/r/memes/top.json")
+    .then(res => res.json())
+    .then(json => json)
+    .catch(err => console.err(err));
+});
