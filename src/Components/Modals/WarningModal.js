4bf5b9a61f64698ccbb357b0e94f77ad0355cd4e
Richard Guerre <guerre.richard@me.com>
2020-05-07 17:54:12 +0200

    fixed typo

--- src/Components/Modals/WarningModal.js
+++ src/Components/Modals/WarningModal.js
@@ -6,11 +6,11 @@ const WarningModal = ({show, setShow, handleFinish}) => {
   return (
     <Modal dialogClassName="laungerModal" show={show} onHide={() => setShow(false)}>
       <Modal.Header closeButton>
-        <Modal.Title>Time's up!<br/>Don't procrastinate, you will loose points!</Modal.Title>
+        <Modal.Title>Time's up!<br/>Don't procrastinate, you will lose points!</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        You will <strong>loose</strong> 10 points every 10 seconds,
-        and <strong>loose</strong> 100 points every 30 seconds.
+        You will <strong>lose</strong> 10 points every 10 seconds,
+        and <strong>lose</strong> 100 points every 30 seconds.
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={handleFinish}>
