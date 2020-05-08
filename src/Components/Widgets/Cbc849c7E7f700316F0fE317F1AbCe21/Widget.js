b186860853a6dca51c7454aada9e6e8c929cd1be
Richard Guerre <guerre.richard@me.com>
2020-05-08 14:07:24 +0200

    code refactor Cleaned up code by removing uneccessary states removed call to firebase functions helloWorld

--- src/Components/Widgets/Cbc849c7E7f700316F0fE317F1AbCe21/Widget.js
+++ src/Components/Widgets/Cbc849c7E7f700316F0fE317F1AbCe21/Widget.js
@@ -1,24 +1,9 @@
 import React, { useState, useEffect } from "react";
-import firebase from "firebase/app";
 
 // import "./Widget.css";
 
 const Widget = () => {
-  const [title, setTitle] = useState('');
-  const [imgSrc, setImgSrc] = useState('');
-  const [index, setIndex] = useState(0);
   const [memes, setMemes] = useState([]);
-
-  const helloWorld = firebase.functions().httpsCallable("helloWorld");
-
-  useEffect(() => {
-    helloWorld()
-      .then((res) => {
-        const {data} = res;
-        console.log(data);
-      })
-      .catch(err => console.log(err));
-  }, [])
 
   const fetchMeme = () => {
     fetch("https://meme-api.herokuapp.com/gimme/30")
@@ -26,34 +11,17 @@ const Widget = () => {
       .then(res => {
         console.log(res);
         setMemes(res.memes);
-        setTitle(res.memes[index].title);
-        setImgSrc(res.memes[index].url);
       })
       .catch(err => console.log(err));
-    }
-    
-  const updateMeme = () => {
-    let idx = index;
-    if (idx < memes.length - 1) ++idx;
-    else idx = 0;
-    setIndex(idx);
-    if (idx !== 0) {
-      setTitle(memes[idx].title);
-      setImgSrc(memes[idx].url);
-    } else {
-      fetchMeme();
-    }
   }
 
   useEffect(() => {
     fetchMeme();
+    // eslint-disable-next-line
   }, []);
 
   return (
     <div className="Widget">
-      {/* <h1 style={{color: 'var(--primary-text-color)'}}>{title}</h1>
-      <img src={imgSrc} alt="Meme" />
-      <input type="button" value="→" onClick={updateMeme} /> */}
       {memes.map((meme, i) => {
         return (
           <span key={i}><Meme title={meme.title} imgSrc={meme.url} src={meme.postLink} /></span>
@@ -66,9 +34,9 @@ const Widget = () => {
 const Meme = ({title = "ERROR", imgSrc, src}) => {
   return (
     <div className="Meme">
-      <a href={src} target="_blank"><h1 style={{color: 'var(--primary-text-color)'}}>{title}</h1></a>
+      <a href={src} target="_blank" rel="noopener noreferrer"><h1 style={{color: 'var(--primary-text-color)'}}>{title}</h1></a>
       <img src={imgSrc} alt={`meme of ${title}`} style={{width: "100%", maxWidth: "500px"}}/>
-      <a href={src} target="_blank">Open in Reddit</a>
+      <a href={src} target="_blank" rel="noopener noreferrer">Open in Reddit</a>
     </div>
   )
 }
