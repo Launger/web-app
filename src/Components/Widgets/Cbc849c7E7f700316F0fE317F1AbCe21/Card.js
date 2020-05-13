a19dc7cdbfa1225be98577ea159bb235c8cc1b3f
Richard Guerre <guerre.richard@me.com>
2020-05-13 13:30:54 +0200

    added meme widget fg

--- src/Components/Widgets/Cbc849c7E7f700316F0fE317F1AbCe21/Card.js
+++ src/Components/Widgets/Cbc849c7E7f700316F0fE317F1AbCe21/Card.js
@@ -2,34 +2,30 @@ import React from "react";
 
 // import config from "../widgetConfig";
 import Template from "../Templates/Reddit-Widget/Card";
+import config from "../widgetConfig";
 // import "./Card.css";
-
-const MemeCardTemplate = ({ id, thumbnails, alreadyGot }) => {
-  const
-    TemplateThumbnails = {}
-
-  return (
-    <div className="meme-card-template">
-      <Template
-        id={id}
-        thumbnails={TemplateThumbnails}
-        alreadyGot={alreadyGot}
-      />
-    </div>
-  );
-};
 
 const Card = ({ alreadyGot }) => {
   const 
     id = "Cbc849c7E7f700316F0fE317F1AbCe21",
+    name = config[id].name,
     thumbnails = {
       bg: null,
-      fg: null,
+      fg: (
+        <>
+          <img
+            style={{position: "relative"}}
+            src="https://styles.redditmedia.com/t5_2qjpg/styles/bannerBackgroundImage_b0ojh4ocr2241.png"
+            alt="Meme illustration"
+          />
+          <h1 style={{color: "white", fontWeight: "700", position: "absolute", textAlign: "center", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}}>{name}</h1>
+        </>
+      ),
     }
 
   return (
-    <div className={`Card-id`}>
-      <MemeCardTemplate
+    <div className={`Card-${id}`}>
+      <Template
         id={id}
         thumbnails={thumbnails}
         alreadyGot={alreadyGot}
