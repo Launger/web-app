19fe6ce710a1d4ec898da07e1f9252d6d633aef7
Richard Guerre <guerre.richard@yahoo.com>
2020-06-27 18:45:27 +0200

    added .env file + Moved firebase config to .env + Added deploy:default:hosting script + Updated README.md

--- src/Helpers/Firebase.js
+++ src/Helpers/Firebase.js
@@ -4,15 +4,17 @@ import "firebase/firestore";
 import "firebase/analytics";
 import "firebase/functions";
 
-app.initializeApp({  
-  apiKey: "AIzaSyBIHQZmwFHF2DiiYc1WfUIRh0sYTKuN9Sk",
-  authDomain: "time-25.firebaseapp.com",
-  databaseURL: "https://time-25.firebaseio.com",
-  projectId: "time-25",
-  storageBucket: "time-25.appspot.com",
-  messagingSenderId: "87335843117",
-  appId: "1:87335843117:web:bc8fae2f19162800ca4fa4",
-  measurementId: "G-E7NKFWME5Z"
+const config = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
+
+app.initializeApp({
+  apiKey: config.apiKey,
+  authDomain: config.authDomain,
+  databaseURL: config.databaseURL,
+  projectId: config.projectId,
+  storageBucket: config.storageBucket,
+  messagingSenderId: config.messagingSenderId,
+  appId: config.appId,
+  measurementId: config.measurementId,
 });
 
 app.analytics();
